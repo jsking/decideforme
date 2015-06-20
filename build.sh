31b4b494667565d52896d257280f2486f61557aa
jsking <justinscottking@gmail.com>
2015-06-19 23:51:09 -0700

    Revision 3

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+vulcanize --inline-scripts --inline-css main.html > index.html 
--- main.js
+++ main.js
@@ -1,18 +1,8 @@
 var curGroup = null;
 
 function setUpElements() {
-	if(localStorage.getItem("groups") != null) {
-		var groups = JSON.parse(localStorage.getItem("groups")).groups;
-		document.getElementById("groups").innerHTML = "";
-		for(var x = 0; x<groups.length; x++) {
-			var tempItem = document.getElementById("baseItem").cloneNode(true);
-			var tempDivider = document.getElementById("divider").cloneNode(true);
-			tempItem.style.display = "flex";
-			tempDivider.style.display = "block";
-			tempItem.children[0].innerHTML = groups[x].name + "<br>" + groups[x].description;
-			document.getElementById("groups").appendChild(tempDivider);
-			document.getElementById("groups").appendChild(tempItem);
-		}
+	if(localStorage.groups != null) {
+		updateGroups();
 	} else {
 		var groups = JSON.stringify({groups: []});
 		localStorage.groups = groups;
@@ -31,47 +21,47 @@ function goToAddItem() {
 function goToTappedItem(e) {
 	if (!e)
     	e = window.event;
-	var curItem = e.srcElement || e.target;
-    curItem = curItem.parentNode;
-	for (var index=0,index=curItem; index = index.previousSibling; ++index);
+	e.stopPropagation();
 	
+	var curItem = e.srcElement || e.target;
+    //curItem = curItem.parentNode;
+	
+	curGroup = Array.prototype.indexOf.call(curItem.parentNode.children, curItem);
+	
+	curGroup = ((curGroup + 1) / 2) - 1;
+	
+	updateItems();
+	
+	document.getElementById('pager').select("1");
+}
+
+function backToGroups() {
+	document.getElementById('pager').select("0");
+}
+
+function backToItems() {
+	document.getElementById('pager').select("1");
 }
 
 function addGroup() {
-	var groups = JSON.parse(localStorage.getItem("groups"));
+	var groups = JSON.parse(localStorage.groups);
 	groups.groups.push({"name": document.getElementById("groupNameInput").value, "description": document.getElementById("groupDescriptionInput").value, "items": []});
-	localStorage.setItem("groups", JSON.stringify(groups));
+	localStorage.groups = JSON.stringify(groups);
 	document.getElementById("groups").innerHTML = "";
 	
-	groups = JSON.parse(localStorage.getItem("groups")).groups;
-	for(var x = 0; x<groups.length; x++) {
-		var tempItem = document.getElementById("baseItem").cloneNode(true);
-		var tempDivider = document.getElementById("divider").cloneNode(true);
-		tempItem.style.display = "flex";
-		tempDivider.style.display = "block";
-		tempItem.children[0].innerHTML = groups[x].name + "<br>" + groups[x].description;
-		document.getElementById("groups").appendChild(tempDivider);
-		document.getElementById("groups").appendChild(tempItem);
-	}
+	updateGroups();
+	
 	document.getElementById('pager').select("0");
 }
 
 function addItem(){
-	var items = JSON.parse(localStorage.getItem("groups"));
+	var items = JSON.parse(localStorage.groups);
 	items.groups[curGroup].items.push({"name": document.getElementById("itemNameInput").value, "description": document.getElementById("itemDescriptionInput").value});
-	localStorage.setItem("groups", JSON.stringify(items));
+	localStorage.groups = JSON.stringify(items);
 	document.getElementById("groups").innerHTML = "";
 	
-	groups = JSON.parse(localStorage.getItem("groups")).groups[curGroup].items;
-	for(var x = 0; x<groups.length; x++) {
-		var tempItem = document.getElementById("baseItem").cloneNode(true);
-		var tempDivider = document.getElementById("divider").cloneNode(true);
-		tempItem.style.display = "flex";
-		tempDivider.style.display = "block";
-		tempItem.children[0].innerHTML = groups[x].name + "<br>" + groups[x].description;
-		document.getElementById("groups").appendChild(tempDivider);
-		document.getElementById("groups").appendChild(tempItem);
-	}
+	updateItems();
+	
 	document.getElementById('pager').select("1");
 }
 
@@ -79,24 +69,64 @@ function deleteCurrentItem(e) {
 	if (!e)
         e = window.event;
 	var deleteThis = e.srcElement || e.target;
-	deleteThis = deleteThis.parentNode;
-	for (var index=0,index=deleteThis; index = index.previousSibling; ++index);
-	if(index != -1) {
-		index = ((index + 1) / 2)-1;
+	
+	deleteThis = deleteThis.parent;
+	
+	var index = Array.prototype.indexOf.call(deleteThis.parent.children, deleteThis);
+	
+	index = ((index + 1) / 2) - 1;
+	
+	if(deleteThis.parent.id == "groups") {
 		var groups = JSON.parse(localStorage.groups);
 		groups.groups.splice(index, 1);
 		localStorage.groups = JSON.stringify(groups);
 		
-		groups = groups.groups;
-		document.getElementById("groups").innerHTML = "";
-		for(var x = 0; x<groups.length; x++) {
-			var tempItem = document.getElementById("baseItem").cloneNode(true);
-			var tempDivider = document.getElementById("divider").cloneNode(true);
-			tempItem.style.display = "block";
-			tempDivider.style.display = "block";
-			tempItem.children[0].innerHTML = groups[x].name + "<br>" + groups[x].description;
-			document.getElementById("groups").appendChild(tempDivider);
-			document.getElementById("groups").appendChild(tempItem);
-		}
+		updateGroups();
+	} else {
+		var groups = JSON.parse(localStorage.groups);
+		groups.groups[curGroup].items.splice(index, 1);
+		localStorage.groups = JSON.stringify(groups);
+		
+		updateItems();
+	}
+	
+	document.getElementById('pager').select("1");
+}
+
+//Randomly choose an item
+
+function chooseRandom() {
+	
+}
+
+//Update functions
+
+function updateGroups() {
+	var groups = JSON.parse(localStorage.groups).groups;
+	document.getElementById("groups").innerHTML = "";
+	for(var x = 0; x<groups.length; x++) {
+		var tempItem = document.getElementById("baseItem").cloneNode(true);
+		var tempDivider = document.getElementById("divider").cloneNode(true);
+		tempItem.style.display = "block";
+		tempDivider.style.display = "block";
+		tempItem.children[0].children[0].innerHTML = groups[x].name;
+		tempItem.children[0].children[1].innerHTML = groups[x].description;
+		document.getElementById("groups").appendChild(tempDivider);
+		document.getElementById("groups").appendChild(tempItem);
+	}
+}
+
+function updateItems() {
+	var groups = JSON.parse(localStorage.groups).groups[curGroup].items;
+	document.getElementById("items").innerHTML = "";
+	for(var x = 0; x<groups.length; x++) {
+		var tempItem = document.getElementById("baseItem").cloneNode(true);
+		var tempDivider = document.getElementById("divider").cloneNode(true);
+		tempItem.style.display = "flex";
+		tempDivider.style.display = "block";
+		tempItem.children[0].children[0].innerHTML = groups[x].name;
+		tempItem.children[0].children[1].innerHTML = groups[x].description;
+		document.getElementById("items").appendChild(tempDivider);
+		document.getElementById("items").appendChild(tempItem);
 	}
 }
