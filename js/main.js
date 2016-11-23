18606bec07a25860c8721e7b57bcdd54a7495eeb
jsking <justinscottking@gmail.com>
2016-11-23 14:47:10 -0800

    Added undo for deleting groups and items

--- js/main.js
+++ js/main.js
@@ -78,22 +78,46 @@ function addGroup(title, description) {
 	var item = document.createElement("li");
 	item.className = "mdl-list__item mdl-list__item--three-line";
 	
-	item.innerHTML = "<span class=\"mdl-list__item-primary-content\" onclick=\"loadGroup(this.parentNode);\"><span>"+title+"</span><span class=\"mdl-list__item-text-body\">"+description+"</span></span><span class=\"mdl-list__item-secondary-content center_vertical\" onclick=\"removeGroup(this.parentNode);\"><a class=\"mdl-list__item-secondary-action\" href=\"\"><i class=\"material-icons\">delete</i></a></span>";
+	item.innerHTML = "<span class=\"mdl-list__item-primary-content\" onclick=\"loadGroup(this.parentNode);\"><span>"+title+"</span><span class=\"mdl-list__item-text-body\">"+description+"</span></span><span class=\"mdl-list__item-secondary-content center_vertical\" onclick=\"deleteGroupClicked(this.parentNode);\"><a class=\"mdl-list__item-secondary-action\"><i class=\"material-icons\">delete</i></a></span>";
 	
 	list.appendChild(item);
 }
 
-function removeGroup(listItem) {
-	var child = listItem;
-	var i = -1;
-	while( (child = child.previousSibling) != null ) 
-		i++;
-	
-	groups.splice(i, 1);
+function removeGroup(listItem, index) {
+	groups.splice(index, 1);
 	localStorage.groups = JSON.stringify(groups);
 	
 	listItem.remove();
 }
+
+function deleteGroupClicked(listItem) {
+	var snackbarContainer = document.getElementById("undoDeleteGroupSnackbar");
+	
+	var index = findIndexInItems(listItem);
+	
+	listItem.style.display = "none";
+	
+	var removeGroupTimeout = setTimeout((function(){removeGroup(listItem, index);}), 5000);
+	
+	var undo = function() {
+		clearTimeout(removeGroupTimeout);
+		listItem.style.display = "flex";
+		snackbarContainer.classList.remove("mdl-snackbar--active");
+		snackbarContainer['aria-hidden'] = true;
+	}
+	var data = {
+		message: "Deleted group \""+listItem.children[0].children[0].innerText+"\".",
+		timeout: 5000,
+		actionHandler: undo,
+		actionText: 'Undo'
+	};
+	snackbarContainer.MaterialSnackbar.showSnackbar(data);
+}
+
+
+
+
+// Load items into items view
 
 function loadGroup(groupItem) {
 	document.getElementById("itemsList").innerHTML="";
@@ -110,6 +134,9 @@ function loadGroup(groupItem) {
 	window.location.hash = "#items";
 	document.getElementById("header_title").innerText = groups[currentIndex].title;
 }
+
+
+// Items commands
 
 function addItemDialog() {
 	var titleText = document.getElementById("itemTitle").value;
@@ -136,27 +163,66 @@ function addItem(title, description) {
 	var item = document.createElement("li");
 	item.className = "mdl-list__item mdl-list__item--three-line";
 	
-	item.innerHTML = "<span class=\"mdl-list__item-primary-content\"><span>"+title+"</span><span class=\"mdl-list__item-text-body\">"+description+"</span></span><span class=\"mdl-list__item-secondary-content center_vertical\" onclick=\"removeItem(this.parentNode);\"><a class=\"mdl-list__item-secondary-action\"><i class=\"material-icons\">delete</i></a></span>";
+	item.innerHTML = "<span class=\"mdl-list__item-primary-content\"><span>"+title+"</span><span class=\"mdl-list__item-text-body\">"+description+"</span></span><span class=\"mdl-list__item-secondary-content center_vertical\" onclick=\"deleteItemClicked(this.parentNode);\"><a class=\"mdl-list__item-secondary-action\"><i class=\"material-icons\">delete</i></a></span>";
 	
 	list.appendChild(item);
 }
 
-function removeItem(listItem) {
-	var child = listItem;
-	var i = -1;
-	while( (child = child.previousSibling) != null ) 
-		i++;
-	
-	groups[currentIndex].items.splice(i, 1);
+function removeItem(listItem, index) {
+	groups[currentIndex].items.splice(index, 1);
 	localStorage.groups = JSON.stringify(groups);
 	
 	listItem.remove();
 }
 
+
+function deleteItemClicked(listItem) {
+	var snackbarContainer = document.getElementById("undoDeleteItemSnackbar");
+	var child = listItem;
+	var i = -1;
+	while( (child = child.previousSibling) != null ) 
+		i++;
+	
+	listItem.style.display = "none";
+	
+	var removeItemTimeout = setTimeout((function(){removeItem(listItem, i);}), 3000);
+	
+	var undo = function() {
+		clearTimeout(removeItemTimeout);
+		listItem.style.display = "flex";
+		snackbarContainer.classList.remove("mdl-snackbar--active");
+		snackbarContainer['aria-hidden'] = true;
+	}
+	var data = {
+		message: "Deleted item \""+listItem.children[0].children[0].innerText+"\".",
+		timeout: 3000,
+		actionHandler: undo,
+		actionText: 'Undo'
+	};
+	snackbarContainer.MaterialSnackbar.showSnackbar(data);
+}
+
+
 function chooseItemRandom() {
-	var randomIndex = Math.floor(Math.random() * groups[currentIndex].items.length);
-	var randomItem = groups[currentIndex].items[randomIndex];
-	document.getElementById("random_title").innerText = randomItem.title;
-	document.getElementById("random_description").innerText = randomItem.description;
-	window.location.hash = "#random_item";
+	var randomLength = groups[currentIndex].items.length;
+	if(randomLength>1) {
+		var randomIndex = Math.floor(Math.random() * randomLength);
+		var randomItem = groups[currentIndex].items[randomIndex];
+		document.getElementById("random_title").innerText = randomItem.title;
+		document.getElementById("random_description").innerText = randomItem.description;
+		window.location.hash = "#random_item";
+	} else {
+		document.getElementById("undoDeleteItemSnackbar").MaterialSnackbar.showSnackbar({message: "You must add more than 2 items!"});
+	}
+}
+
+
+
+function findIndexInItems(item) {
+	var child = item;
+	var i = -1;
+	while( (child = child.previousSibling) != null ) 
+		i++;
+	
+	return i;
 }
